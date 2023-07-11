4d6290f966b47981bff868dc18fd6373606ec8a7
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-07-11 15:43:56 +0530

    [sanjana] Add : Insert Columns and update data

--- Customer_Table.sql
+++ Customer_Table.sql
@@ -16,3 +16,12 @@ Insert into Customer_Table (Name,Phone,Address) values ('A','96538965555','1465'
 Insert into Customer_Table (Name,Phone,Address) values ('E','89531365555','1466')
 Insert into Customer_Table (Name,Phone,Address) values ('F','96544365555','1467')
 Select * from Customer_Table
+--UC4
+Alter table  Customer_Table add  Country varchar(20),Salary Bigint,Pincode Bigint
+Update Customer_Table set  Country='India',Salary = 200000,Pincode =416012 where ID=1
+Update Customer_Table set  Country='India',Salary = 250000,Pincode =416025 where ID=2
+Update Customer_Table set  Country='US',Salary = 700000,Pincode =51212 where ID=3
+Update Customer_Table set  Country='Canada',Salary = 900000,Pincode =85123 where ID=4
+Update Customer_Table set  Country='India',Salary = 350000,Pincode =414512 where ID=5
+Update Customer_Table set  Country='Japan',Salary = 500000,Pincode =98623 where ID=6
+Update Customer_Table set  Country='Germany',Salary = 200000,Pincode =74562 where ID=7
